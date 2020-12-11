7124e11451bb0a50200003b85becd3da893cda1b
Sam <109807@st-maartenscollege.nl>
2020-12-11 09:34:24 +0100

    Add project files.

---
+++ BeeldHerkenning1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BeeldHerkenning1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ BeeldHerkenning1ML.ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+using System;
+using BeeldHerkening1ML.Model;
+namespace BeeldHerkening1
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Debug.WriteLine(" Object is not valid for this category.");
+            // Add input data
+            var input = new ModelInput();
+            input.ImageSource = "C:\\Users\\pc-5\\Downloads\\Object-recognition-CIFAR-10-master\\rocva ml.NET images\\autos\\auto1.jpg";
+
+            // Load model and predict output of sample data
+            ModelOutput result = ConsumeModel.Predict(input);
+            Debug.WriteLine(result.Prediction);
+            Console.WriteLine(result.Prediction);
+        }
+    }
+}
+
+
